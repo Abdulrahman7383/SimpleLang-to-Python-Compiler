4a03631a3150880d24f73ca4b4d0966a2d2030d4
Abdulrahman7383 <engabdulrahmanalfkih1@gmail.com>
2026-05-31 17:06:50 +0300

    Add files via upload

---
+++ output-checkpoint.py
@@ -0,0 +1,11 @@
+# === كود بايثون تم توليده بواسطة مترجمك الخاص ===
+x = 10
+y = 5
+if x > y:
+    print("x is bigger")
+else:
+    print("y is bigger")
+counter = 0
+while counter < 3:
+    print(counter)
+    counter = (counter + 1)
